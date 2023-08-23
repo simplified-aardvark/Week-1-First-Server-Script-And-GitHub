bdaa271bddc78a1b47f70a859b2e1353d1269010
bswain47 <5828014-bswain47@users.noreply.replit.com>
2023-08-23 05:28:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let my_http = require("http");
+
+let my_server = my_http.createServer(
+  function(my_request, my_response) {
+    console.log( my_request.url);
+
+    let my_text;
+
+    if (my_request.url === "/england-plus") {
+      my_text = "It's Aluminium!";
+    } else if (my_request.url == "/usa-canada") {
+      my_text = "It's Aluminum!";
+    } else {
+      my_text = "What?";
+    }
+
+    my_response.writeHead(200, {"Content-Type": "text/plain"});
+
+    my_response.end(my_text);
+  }
+)
+
+my_server.listen(8080, "0.0.0.0");
+
+console.log("Server has started!");
